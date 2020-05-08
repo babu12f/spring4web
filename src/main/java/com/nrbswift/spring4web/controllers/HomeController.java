6a75278aa526fb29ab9af77460312f5f3305ee20
Shofiullah Babor <babu_12f@yahoo.com>
2020-05-08 21:54:01 +0600

    send and receive message form queue

--- src/main/java/com/nrbswift/spring4web/controllers/HomeController.java
+++ src/main/java/com/nrbswift/spring4web/controllers/HomeController.java
@@ -1,15 +1,46 @@
 package com.nrbswift.spring4web.controllers;
 
+import com.nrbswift.spring4web.mq.MessageReceiver;
+import com.nrbswift.spring4web.mq.MessageSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @Autowired
+    MessageSender messageSender;
+
+    @Autowired
+    MessageReceiver messageReceiver;
+
     @RequestMapping("/")
     public String showHome(Model model) {
         model.addAttribute("name", "Shofiullah babor");
 
         return "home";
+    }
+
+    @RequestMapping("/send")
+    @ResponseBody
+    public String sendMessageToQueue(@RequestParam(value = "msg", required = false) String message) {
+        if (message == null) {
+            messageSender.sendMessage("any message");
+        }
+        else {
+            messageSender.sendMessage(message);
+        }
+
+        return "message add Successfully :)";
+    }
+    @RequestMapping("/receive")
+    @ResponseBody
+    public String receiveMessageToQueue() {
+        String message = messageReceiver.receiveMessage();
+
+        return "message from queue : >>>> " + message;
     }
 }
---
+++ src/main/java/com/nrbswift/spring4web/mq/MessageReceiver.java
@@ -0,0 +1,29 @@
+package com.nrbswift.spring4web.mq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Component
+public class MessageReceiver {
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Autowired
+    MessageConverter messageConverter;
+
+    public String receiveMessage() {
+        try {
+            Message message = jmsTemplate.receive();
+            String response = (String) messageConverter.fromMessage(message);
+            return response;
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
