a456f75b6661794578174f644bf21ef54b2f46b9
Shofiullah Babor <babu_12f@yahoo.com>
2020-05-16 09:01:56 +0600

    clean project

--- src/main/java/com/nrbswift/spring4web/controllers/HomeController.java
+++ src/main/java/com/nrbswift/spring4web/controllers/HomeController.java
@@ -1,7 +1,5 @@
 package com.nrbswift.spring4web.controllers;
 
-import com.nrbswift.spring4web.dao.User;
-import com.nrbswift.spring4web.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,22 +10,8 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    @Autowired
-    UserDAO userDao;
-
     @RequestMapping("/")
     public String showHome(Model model) {
-
-//        User u = new User("admin2", "shofiullah babor", "babu@gmal.com", "secret", true);
-//        userDao.createUser(u);
-
-        List<User> users = userDao.getAll();
-
-        for (User user:users) {
-            System.out.println(user);
-        }
-
-//        model.addAttribute("users", users);
         model.addAttribute("name", "Shofiullah babor");
 
         return "home";
